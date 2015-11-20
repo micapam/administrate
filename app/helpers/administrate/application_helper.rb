fadf8cf538c15e0643a2877244fa5fd7487fd4bf
Eric Dias <edias@medivector.com>
2015-11-20 13:09:45 -0500

    Adds a helper to make has_many/show use it's own resource's collection partial

--- app/helpers/administrate/application_helper.rb
+++ app/helpers/administrate/application_helper.rb
@@ -4,5 +4,13 @@ module Administrate
       locals.merge!(field: field)
       render locals: locals, partial: field.to_partial_path
     end
+
+    def collection_partial_path(field)
+      if File.exist?("#{field.resources[0].class.to_s.underscore}/collection")
+        "#{field.resources[0].class.to_s.underscore}/collection"
+      else
+        'administrate/application/collection'
+      end
+    end
   end
 end
